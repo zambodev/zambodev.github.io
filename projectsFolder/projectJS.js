bddc7a0befe13ea599542f8db0e2af3820fc3410
GiovanniNeve <thomasz01@outlook.it>
2020-10-31 21:57:36 +0100

    first commit

---
+++ projectsFolder/projectJS.js
@@ -0,0 +1,31 @@
+var mainTable = document.getElementById("mainTable");
+
+async function getData() {
+    let response = await fetch(`https://api.github.com/users/GiovanniNeve/repos`);
+    let obj = await response.json();
+
+    return obj;
+}
+
+getData()
+    .then((obj) => {
+
+        while (mainTable.rows[1]) {
+            mainTable.rows[1].remove();
+        }
+        for (var index1 = 1; index1 < obj.length + 1; index1++) {
+            let row = mainTable.insertRow(index1);
+            let cell = new Array(6);
+            cell[index1] = row.insertCell();
+            cell[index1].innerHTML = obj[index1 - 1].name;
+            cell[index1] = row.insertCell();
+            cell[index1].innerHTML = obj[index1 - 1].language;
+
+            var str = "GitHub";
+            var a = str.link(obj[index1 - 1].html_url);
+
+            cell[index1] = row.insertCell();
+            cell[index1].innerHTML = a;
+
+        }
+    });
