b050f039bf04c70dceb6a4cd1c5200ba5b2b9ec5
GiovanniNeve <thomasz01@outlook.it>
2021-01-13 19:50:52 +0100

    Contributors js fix

--- project.js
+++ project.js
@@ -8,54 +8,59 @@ async function getData(link) {
     return obj;
 }
 
-if(location.href.split('/').pop() != "index.html") {
+function update() {
 
-    getData(`https://api.github.com/users/Zambo-dev/repos`)
+    if(location.href.split('/').pop() != "index.html") {
+
+        getData(`https://api.github.com/users/Zambo-dev/repos`)
+            .then((obj) => {
+                
+                while (mainTable.rows[1]) {
+                    mainTable.rows[1].remove();
+                }
+                for (var index1 = 1; index1 < obj.length + 1; index1++) {
+                    let row = mainTable.insertRow(index1);
+                    let cell = new Array(6);
+                    cell[index1] = row.insertCell();
+                    cell[index1].innerHTML = obj[index1 - 1].name;
+                    cell[index1] = row.insertCell();
+                    
+                    if(obj[index1 - 1].language == null) {
+                        cell[index1].innerHTML = "No language";
+                    } else {
+                        cell[index1].innerHTML = obj[index1 - 1].language;
+                    }
+
+                    var str = "GitHub";
+                    var a = str.link(obj[index1 - 1].html_url);
+
+                    cell[index1] = row.insertCell();
+                    cell[index1].innerHTML = a;
+
+                }
+            });
+
+    } else {
+
+        getData(`https://api.github.com/repos/Zambo-dev/Zambo-dev.github.io/contributors`)
         .then((obj) => {
             
-            while (mainTable.rows[1]) {
-                mainTable.rows[1].remove();
-            }
-            for (var index1 = 1; index1 < obj.length + 1; index1++) {
-                let row = mainTable.insertRow(index1);
-                let cell = new Array(6);
-                cell[index1] = row.insertCell();
-                cell[index1].innerHTML = obj[index1 - 1].name;
-                cell[index1] = row.insertCell();
+            for(var index = 0; index < obj.length; index++) {
+            
+                var a = document.createElement('a');
+                var img = document.createElement('img');
+                img.setAttribute('src', obj[index].avatar_url);
+                img.setAttribute('alt', obj[index].login);
+                img.setAttribute('title', obj[index].login);
+                a.setAttribute('href', obj[index].html_url);
+                a.appendChild(img);
+                document.getElementById("images").appendChild(a);
                 
-                if(obj[index1 - 1].language == null) {
-                    cell[index1].innerHTML = "No language";
-                } else {
-                    cell[index1].innerHTML = obj[index1 - 1].language;
-                }
-
-                var str = "GitHub";
-                var a = str.link(obj[index1 - 1].html_url);
-
-                cell[index1] = row.insertCell();
-                cell[index1].innerHTML = a;
-
             }
+
         });
 
-} else {
- 
-    getData(`https://api.github.com/repos/Zambo-dev/Zambo-dev.github.io/contributors`)
-    .then((obj) => {
-        
-        for(var index = 0; index < obj.length; index++) {
-        
-            var a = document.createElement('a');
-            var img = document.createElement('img');
-            img.setAttribute('src', obj[index].avatar_url);
-            img.setAttribute('alt', obj[index].login);
-            img.setAttribute('title', obj[index].login);
-            a.setAttribute('href', obj[index].html_url);
-            a.appendChild(img);
-            document.getElementById("images").appendChild(a);
-            
-        }
-
-    });
-
+    }
 }
+
+update();
