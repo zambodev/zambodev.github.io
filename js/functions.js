5359939108a25c2e87e89df0ef1bd5aa9e26868b
Zambo-dev <thomasz01@outlook.it>
2022-05-04 18:15:23 +0200

    Rebuilt the whole website

--- js/functions.js
+++ js/functions.js
@@ -7,26 +7,26 @@ async function getData(link) {
 
 function parseDate(date) {
     const month = date.getMonth() + 1;
-    const data_str = date.getUTCDate() + "-" + month + "-" + date.getFullYear();
+    const data_str = date.getUTCDate() + "/" + month + "/" + date.getFullYear().toString().slice(2);
     return data_str;
 }
 
 function isMobile() {
-    if(window.innerWidth <= 800)
+    if (window.innerWidth <= 800)
         return true;
     else
         return false;
-  }
+}
 
 function sort(obj) {
-    for(var i=0; i < obj.length; i++) {
-        for(var j=1; j < obj.length; j++) {
+    for (var i = 0; i < obj.length; i++) {
+        for (var j = 1; j < obj.length; j++) {
             var t1 = new Date(obj[j].updated_at);
-            var t2 = new Date(obj[j-1].updated_at);
-            if(t1 > t2) {
+            var t2 = new Date(obj[j - 1].updated_at);
+            if (t1 > t2) {
                 var tmp = obj[j];
-                obj[j] = obj[j-1];
-                obj[j-1] = tmp;
+                obj[j] = obj[j - 1];
+                obj[j - 1] = tmp;
             }
         }
     }
@@ -35,13 +35,13 @@ function sort(obj) {
 }
 
 function update() {
-    const mainTable = document.getElementById("mainTable");
+    const mainTable = document.getElementById("project_table");
 
-    if(mainTable !== null) {
+    if (mainTable !== null) {
 
         getData(`https://api.github.com/users/Zambo-dev/repos`)
-            .then((obj) => {    
-                
+            .then((obj) => {
+
                 var obj = sort(obj);
 
                 while (mainTable.rows[1]) {
@@ -50,34 +50,29 @@ function update() {
                 for (var index1 = 1; index1 < obj.length + 1; index1++) {
                     let row = mainTable.insertRow(index1);
                     let cell = new Array(6);
+
+                    /* Project name */
+                    var link = document.createElement("a");
+                    link.setAttribute("href", obj[index1 - 1].html_url);
+                    link.setAttribute("rel", "noopener noreferrer");
+                    var linkText = document.createTextNode(obj[index1 - 1].name);
+                    link.appendChild(linkText);
+
                     cell[index1] = row.insertCell();
-                    cell[index1].innerHTML = obj[index1 - 1].name;
+                    cell[index1].appendChild(link);
+
+
+                    /* Language */
                     cell[index1] = row.insertCell();
-                    
-                    if(obj[index1 - 1].language == null) {
+                    if (obj[index1 - 1].language == null) {
                         cell[index1].innerHTML = "No language";
                     } else {
                         cell[index1].innerHTML = obj[index1 - 1].language;
                     }
-                    
-                    if(!isMobile()) {
-                        cell[index1] = row.insertCell();
-                        cell[index1].innerHTML = parseDate(new Date(obj[index1 -1].updated_at));
-                    }
-                    else
-                    {
-                        var last_commit = document.getElementById("commit");
-                        last_commit.style.display = "none";
-                        
-                        var language = document.getElementById("lang");
-                        language.innerHTML = "Lang";
-                    }
 
-                    var str = "GitHub";
-                    var a = str.link(obj[index1 - 1].html_url);
-
+                    /* Last commit */
                     cell[index1] = row.insertCell();
-                    cell[index1].innerHTML = a;
+                    cell[index1].innerHTML = parseDate(new Date(obj[index1 - 1].updated_at));
 
                 }
             });
@@ -85,24 +80,24 @@ function update() {
     } else {
 
         getData(`https://api.github.com/repos/Zambo-dev/Zambo-dev.github.io/contributors`)
-        .then((obj) => {
-            
-            const contributors = document.getElementById("contributorsFlexbox");
+            .then((obj) => {
 
-            for(var index = 0; index < obj.length; index++) {
-            
-                var a = document.createElement('a');
-                var img = document.createElement('img');
-                img.setAttribute('src', obj[index].avatar_url);
-                img.setAttribute('alt', obj[index].login);
-                img.setAttribute('title', obj[index].login);
-                a.setAttribute('href', obj[index].html_url);
-                a.appendChild(img);
-                document.getElementById("images").appendChild(a);
-                
-            }
+                const contributors = document.getElementById("contributorsFlexbox");
 
-        });
+                for (var index = 0; index < obj.length; index++) {
+
+                    var a = document.createElement('a');
+                    var img = document.createElement('img');
+                    img.setAttribute('src', obj[index].avatar_url);
+                    img.setAttribute('alt', obj[index].login);
+                    img.setAttribute('title', obj[index].login);
+                    a.setAttribute('href', obj[index].html_url);
+                    a.appendChild(img);
+                    document.getElementById("images").appendChild(a);
+
+                }
+
+            });
 
     }
 }
